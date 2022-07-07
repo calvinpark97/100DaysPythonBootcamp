c45ba0a6054011418991cc193757a45a517defaf
calvinpark97 <60866701+calvinpark97@users.noreply.github.com>
2022-07-07 16:24:01 -0500

    Day19 Completd

--- Day19_Instances_States_HigherOrderFunc/Day19_TurtleRace.py
+++ Day19_Instances_States_HigherOrderFunc/Day19_TurtleRace.py
@@ -1,16 +1,40 @@
 from turtle import Turtle, Screen
+import random
 
 screen = Screen()
+race_on = False
 screen.setup(width=500,height=400)
 user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race?").lower()
 colors = ("red", "orange", "yellow", "green", "blue", "violet")
-y_pos = [-290, -60, -30,0, 30, 60, 290]
+y_pos = [-100, -60, -20, 20, 60, 100]
+Racing_Turtles = []
 
 for turtles_color in range (0,6):
-    Racing_Turtle = Turtle(shape="turtle")
-    Racing_Turtle.color(colors[turtles_color])
-    Racing_Turtle.pu()
-    Racing_Turtle.goto(-230, y_pos[turtles_color])
+    New_Turtle = Turtle(shape="turtle")
+    New_Turtle.color(colors[turtles_color])
+    New_Turtle.pu()
+    New_Turtle.goto(-230, y_pos[turtles_color])
+    Racing_Turtles.append(New_Turtle)
+
+if user_bet:
+    race_on = True
+
+while race_on == True:
+    for x in range(0,6):
+        turtle_speed = random.randint(0, 10)
+        Racing_Turtles[x].forward(turtle_speed)
+        if Racing_Turtles[x].xcor() > 250:
+            winning_turtle = Racing_Turtles[x].fillcolor()
+            race_on = False
+
+if user_bet == winning_turtle:
+    print(f"You win! The winning turtle color was {winning_turtle}!")
+else:
+    print(f"You lose! The winning turtle color was {winning_turtle}!")
+
+
+
+
 
 
 
