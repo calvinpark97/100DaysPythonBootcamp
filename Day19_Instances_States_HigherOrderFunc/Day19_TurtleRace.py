d4c05488f3d775354886c4747849bc505abac45f
calvinpark97 <60866701+calvinpark97@users.noreply.github.com>
2022-07-07 14:30:56 -0500

    Day19_updates on turtle race

--- Day19_Instances_States_HigherOrderFunc/Day19_TurtleRace.py
+++ Day19_Instances_States_HigherOrderFunc/Day19_TurtleRace.py
@@ -1,8 +1,17 @@
 from turtle import Turtle, Screen
 
-Timmy = Turtle()
 screen = Screen()
-screen.setup(700,700)
+screen.setup(width=500,height=400)
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race?").lower()
+colors = ("red", "orange", "yellow", "green", "blue", "violet")
+y_pos = [-290, -60, -30,0, 30, 60, 290]
+
+for turtles_color in range (0,6):
+    Racing_Turtle = Turtle(shape="turtle")
+    Racing_Turtle.color(colors[turtles_color])
+    Racing_Turtle.pu()
+    Racing_Turtle.goto(-230, y_pos[turtles_color])
+
 
 
 screen.exitonclick()
