5d768e7741ccb60f7ac193ed95ca0d488a5ab630
calvinpark97 <60866701+calvinpark97@users.noreply.github.com>
2022-07-07 20:29:46 -0500

    Day 20 Started

---
+++ Day20_21_SnakeGame/Snake Game.py
@@ -0,0 +1,21 @@
+from turtle import Turtle, Screen
+from Snake import Snake
+import time
+
+screen = Screen()
+screen.setup(width=500,height=500)
+screen.bgcolor("black")
+screen.title("Snake Game")
+starting_position = [(0,0),(-20,0), (-40,0)]
+game_on = True
+screen.tracer(0)
+
+snake = Snake()
+
+snake.create_snake()
+
+while game_on == True:
+
+    snake.move()
+
+screen.exitonclick()
