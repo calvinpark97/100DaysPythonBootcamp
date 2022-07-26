28332159169beaac217dade6155dabe7c24fcada
calvinpark97 <60866701+calvinpark97@users.noreply.github.com>
2022-07-26 01:29:33 -0500

    Day22
    Pong started

---
+++ Day22_PongGame/PongGame.py
@@ -0,0 +1,13 @@
+from turtle import Turtle, Screen
+import time
+
+screen = Screen()
+screen.setup(width=1000,height=800)
+screen.bgcolor("black")
+screen.title("Pong")
+game_on = True
+screen.tracer(0)
+
+
+
+screen.exitonclick()
