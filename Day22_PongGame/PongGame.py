dcada5dd8f7796e6d9f58ff4c9ad256684d2d07f
calvinpark97 <60866701+calvinpark97@users.noreply.github.com>
2022-07-31 04:14:33 -0500

    Correcting Merge Changes

--- Day22_PongGame/PongGame.py
+++ Day22_PongGame/PongGame.py
@@ -2,48 +2,28 @@ from turtle import Turtle, Screen
 import time
 
 screen = Screen()
-screen.setup(width=800,height=600)
+screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong")
 game_on = True
+
+
 def moveUp():
     if rightPaddle.ycor() > 240:
         pass
     else:
         newy = rightPaddle.ycor() + 20
-        rightPaddle.goto(350,newy)
-<<<<<<< HEAD
-=======
+        rightPaddle.goto(350, newy)
+
 
 def moveDown():
     if rightPaddle.ycor() < -230:
         pass
     else:
         newy = rightPaddle.ycor() - 20
-        rightPaddle.goto(350,newy)
->>>>>>> 41c2a66075d3a46e374d9736075702445e5e6fea
+        rightPaddle.goto(350, newy)
 
-def moveDown():
-    if rightPaddle.ycor() < -230:
-        pass
-    else:
-        newy = rightPaddle.ycor() - 20
-        rightPaddle.goto(350,newy)
 
-def LeftFlipperCreate():
-    LeftFlipper = Turtle()
-    LeftFlipper.shape('square')
-    LeftFlipper.width(10)
-    LeftFlipper.pu()
-    LeftFlipper.goto(490, 0)
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-LeftFlipperCreate()
-=======
-
-=======
->>>>>>> 41c2a66075d3a46e374d9736075702445e5e6fea
 rightPaddle = Turtle()
 rightPaddle.ht()
 rightPaddle.shape("square")
@@ -51,7 +31,7 @@ rightPaddle.color("white")
 rightPaddle.turtlesize(5, 1)
 rightPaddle.pu()
 rightPaddle.ht()
-rightPaddle.setpos(350,0)
+rightPaddle.setpos(350, 0)
 rightPaddle.st()
 screen.listen()
 screen.onkeypress(moveUp, "Up")
@@ -59,5 +39,5 @@ screen.onkeypress(moveDown, "Down")
 
 while game_on == True:
     screen.update()
-    
+
 screen.exitonclick()
