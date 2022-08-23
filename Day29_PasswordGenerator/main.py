ec717e188b0be5d187172544952edbd0a7e7ce52
calvinpark97 <60866701+calvinpark97@users.noreply.github.com>
2022-08-22 22:14:12 -0500

    Day29
    Completed Password generator

--- Day29_PasswordGenerator/main.py
+++ Day29_PasswordGenerator/main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import messagebox
 import random
+import pyperclip
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -20,6 +21,7 @@ def generate_password():
     random.shuffle(rand_password)
     rand_password = "".join(rand_password)
     password_text.insert(0, string=rand_password)
+    pyperclip.copy(rand_password)
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def add_to_file():
     website = website_text.get()
